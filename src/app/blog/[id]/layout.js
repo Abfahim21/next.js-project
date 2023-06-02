17e637345ab5fd71625c604f10e8d01755c11330
Abfahim21 <abrarfahim21@gmail.com>
2023-06-03 03:21:35 +0600

    layout

---
+++ src/app/blog/[id]/layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Bloglayout = ({childern}) => {
+  return (
+    <div>
+        <h2>This is Blog Layout</h2>
+        {childern}
+    </div>
+  )
+}
+
+export default Bloglayout
