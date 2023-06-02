ba6181dd316fd892cc9aa14799b0d3f65b94b3f0
Abfahim21 <abrarfahim21@gmail.com>
2023-06-03 03:42:33 +0600

    components

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,53 @@
+import Link from 'next/link'
 import React from 'react'
+
+const links = [
+  {
+    id:1,
+    title:"Home",
+    url:"/"
+  },
+
+  {
+    id:2,
+    title:"About",
+    url:"/about"
+  },
+  {
+    id:3,
+    title:"Blog",
+    url:"/blog"
+  },
+  {
+    id:4,
+    title:"Portfolio",
+    url:"/portfolio"
+  },
+  {
+    id:5,
+    title:"Dashboard",
+    url:"/dashboard"
+  },
+  {
+    id:6,
+    title:"Contact",
+    url:"/contact"
+  },
+]
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <Link href="/">Abrar Fahim</Link>
+      <div>
+        {links.map(
+          link=>(
+            <Link key={link.id} href={link.url}>{link.title}</Link>
+          )
+        )}
+
+      </div>
+    </div>
   )
 }
 
