7e3ea8ab45a62344f2e0464223025a6119c6fb52
Abfahim21 <abrarfahim21@gmail.com>
2023-06-04 18:02:18 +0600

    images next

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import styles from './footer.module.css'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
     <div className={styles.container}>
       <div>Abrar Fahim @ all rights reserved - 2023</div>
-      <div></div>
+      <div className={styles.social}>
+        <Image src="/1.png" className={styles.icon} width={20} height={20} alt='Abrar Fahim Facebook'/>
+        <Image src="/2.png" className={styles.icon} width={20} height={20} alt='Abrar Fahim Instagram'/>
+        <Image src="/3.png" className={styles.icon} width={20} height={20} alt='Abrar Fahim'/>
+        <Image src="/4.png" className={styles.icon} width={20} height={20} alt='Abrar Fahim'/>
+      </div>
     </div>
   )
 }
