fde93ba64e6a5990d5b26ca2dd65f489e9782def
Abfahim21 <abrarfahim21@gmail.com>
2023-06-04 18:33:01 +0600

    css advanced

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <div className={styles.item}>
-        <h1 className={styles.title}>Better Design for your Digital Product.</h1>
+        <h1 className={styles.title}>Better Design for your Digital Product</h1>
         <p className={styles.desc}>Turning your ideas into reality. We bring together the teams for the global tech industries.</p>
         <button className={styles.button}>See Our Works</button>
       </div>
