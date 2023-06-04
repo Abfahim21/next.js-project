65b42d66995b4544359b353e0bfdc1c151c090bd
Abfahim21 <abrarfahim21@gmail.com>
2023-06-04 18:16:31 +0600

    images next

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,18 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import heroImage from 'public/hero.png'
 
 export default function Home() {
   return (
-    <div >
-      Home
+    <div className={styles.container}>
+      <div className={styles.texts}>
+        <h1>Better Design for your Degital Products</h1>
+        <p>Turning your ideas into reality. We bring together the teams for the global tech industries.</p>
+        <button>See Our Works</button>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src={heroImage}/>
+      </div>
     </div>
   )
 }
