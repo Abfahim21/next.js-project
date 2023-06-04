eb534d96ba0ac8682de8838e1dd6fbb13ca73967
Abfahim21 <abrarfahim21@gmail.com>
2023-06-04 17:43:14 +0600

    images next

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import styles from './footer.module.css'
 
 const Footer = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <div>Abrar Fahim @ all rights reserved - 2023</div>
       <div></div>
     </div>
