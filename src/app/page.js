56afb6e2dd5e84d16fc4fd492637df0d6535af14
Abfahim21 <abrarfahim21@gmail.com>
2023-06-04 18:27:02 +0600

    images next

--- src/app/page.js
+++ src/app/page.js
@@ -5,13 +5,13 @@ import heroImage from 'public/hero.png'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <div className={styles.texts}>
-        <h1>Better Design for your Degital Products</h1>
-        <p>Turning your ideas into reality. We bring together the teams for the global tech industries.</p>
-        <button>See Our Works</button>
+      <div className={styles.item}>
+        <h1 className={styles.title}>Better Design for your Digital Product.</h1>
+        <p className={styles.desc}>Turning your ideas into reality. We bring together the teams for the global tech industries.</p>
+        <button className={styles.button}>See Our Works</button>
       </div>
-      <div className={styles.imgContainer}>
-        <Image src={heroImage}/>
+      <div className={styles.item}>
+        <Image src={heroImage} alt='Hero Image' className={styles.img}/>
       </div>
     </div>
   )
