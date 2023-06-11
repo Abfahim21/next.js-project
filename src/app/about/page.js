677574686d18d8883ad7e07122e53f486cad1aa9
Abfahim21 <abrarfahim21@gmail.com>
2023-06-11 08:08:12 +0600

    hi

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -6,11 +6,33 @@ const About = () => {
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
-      <Image src="/pexels.jpg" fill={true} alt='pexels' className={styles.img}/>
+        <Image src="/pexels.jpg" fill={true} alt='pexels' className={styles.img}/>
+        <div className="imgText">
+          <h1 className={styles.imgTitle}>Image main Text</h1>
+          <p className={styles.imgDesc}>Here are some texts about this image</p>
+        </div>
       </div>
       <div className={styles.textContainer}>
-        <h1 className={styles.imgTitle}>Digital Story tellers</h1>
-        <h2 className={styles.imgDesc}>Handcrafting award winning...</h2>
+        <div className={styles.item}>
+          <h1 className={styles.title}>Who are we?</h1>
+          <p className={styles.desc}>optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+            obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+            nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+            tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+            quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
+            sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
+            recusandae alias error harum maxime adipisci amet laborum. Perspiciatis </p>
+        </div>
+        <div className={styles.item}>
+          <h1 className={styles.title}>Who are we?</h1>
+          <p className={styles.desc}>optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
+            obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
+            nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
+            tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+            quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos 
+            sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
+            recusandae alias error harum maxime adipisci amet laborum. Perspiciatis </p>
+        </div>
       </div>
     </div>
     
