784dc4c49afb936fb1d41047575fa445f8b33826
Abfahim21 <abrarfahim21@gmail.com>
2023-06-11 07:40:41 +0600

    hi

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import styles from './page.module.css'
+import Image from 'next/image'
 
 const About = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.imgContainer}></div>
-      <div className={styles.textContainer}></div>
+      <div className={styles.imgContainer}>
+      <Image src="/pexels.jpg" fill={true} alt='pexels' className={styles.img}/>
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.imgTitle}>Digital Story tellers</h1>
+        <h2 className={styles.imgDesc}>Handcrafting award winning...</h2>
+      </div>
     </div>
+    
   )
 }
 
---
+++ src/components/darkMode/DarkMode.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './DarkMode.module.css'
+
+const DarkMode = () => {
+  return (
+    <div className={styles.container}>DarkMode</div>
+  )
+}
+
+export default DarkMode
